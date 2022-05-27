c87cd8899e084c721706fb32119953db88610586
LeeJaeHyun <myleje@gmail.com>
2022-05-27 15:19:49 +0900

    .

--- Assets/Scripts/PointerManager.cs
+++ Assets/Scripts/PointerManager.cs
@@ -1,11 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
 
-public class PointerManager : MonoBehaviour
+public class PointerManager : MonoBehaviour, IMixedRealityFocusHandler, IMixedRealityPointerHandler
 {
     public Transform heartObject;
     public bool isRotate;
+
+    public void OnFocusEnter(FocusEventData eventData)
+    {
+        Debug.Log("포커스 인");
+    }
+
+    public void OnFocusExit(FocusEventData eventData)
+    {
+        Debug.Log("포커스 아웃");
+    }
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("포인터 클릭 이벤트");
+        isRotate = !isRotate;
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("포인터 다운 이벤트");
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("포인터 드래그 이벤트");
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("포인터 업 이벤트");
+    }
 
     public void ShowMessage(string msg)
     {
